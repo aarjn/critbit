d3b8f95814e99e0e1eb06caf6449270bd174d0ab
4rjunc <arjunmundamani@gmail.com>
2026-03-17 15:31:48 +0530

    feat: add 2 types of Node. Internal, External

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub enum Node {
+    Internal(Box<InternalNode>),
+    External(Vec<u8>),
+}
+
+pub struct InternalNode {
+    pub crit_bit: usize,
+    pub left: Node,
+    pub right: Node,
+}
+
 fn main() {
     println!("Hello, world!");
 }
