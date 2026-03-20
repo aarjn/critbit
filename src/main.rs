687125a9fbad00187a78e9d6d87a8303e4e7e951
4rjunc <arjunmundamani@gmail.com>
2026-03-21 00:33:15 +0530

    fun: find_crit_bit

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,26 @@ impl CritBitTree {
     }
 }
 
+fn find_crit_bit(a: &[u8], b: &[u8]) -> Option<usize> {
+    let min_len = a.len().min(b.len());
+
+    // find the first differing byte
+    for i in 0..min_len {
+        if a[i] != b[i] {
+            let xor = a[i] ^ b[i];
+            let bit_in_bytes = 7 - xor.leading_zeros() as usize;
+            return Some(i * 8 + bit_in_bytes);
+        }
+    }
+
+    // keys differ only in length
+    if a.len() != b.len() {
+        Some(min_len * 8)
+    } else {
+        None
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
